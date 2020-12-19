c734e1dad44154dcb6e86db8288377b15f942841
Mikhail Biltsevich <mikebiltsevich@gmail.com>
2020-12-19 13:28:31 +0300

    feat: implement removeByIndex method

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -38,6 +38,25 @@ class SinglyLinkedList {
     prevTail.next = null;
   }
 
+  removeByIndex(index) {
+    const removedNode = this.getNode(index);
+    if (!removedNode) {
+      console.log('Out of range list. Try to use another index or add node in the end of list');
+      return;
+    }
+
+    if(this.head === removedNode) {
+      this.head = removedNode.next;
+    } else {
+      const prevNode = this.getNode(index - 1);
+      prevNode.next = removedNode.next;
+
+      if (removedNode === this.tail) {
+        this.tail = prevNode;
+      }
+    }
+  }
+
   getNode(index) {
     let tmpIndex = 0;
     let node = this.head;
