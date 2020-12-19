7cc9fcbe8e7c2e9f9ae96108b0acc96c983c9afa
Mikhail Biltsevich <mikebiltsevich@gmail.com>
2020-12-19 18:18:43 +0300

    feat: implement add method

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -4,7 +4,14 @@ class DoublyLinkedList {
   }
 
   add(value) {
-
+    const node = new Node(value);
+    if (!this.head) {
+      this.head = node;
+    } else {
+      this.tail.next = node;
+      node.prev = this.tail;
+    }
+    this.tail = node;
   }
 
   addByIndex(value, index) {
