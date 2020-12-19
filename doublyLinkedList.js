8fccb84104fd9e6d438c4545f3c785425a90ec02
Mikhail Biltsevich <mikebiltsevich@gmail.com>
2020-12-19 18:08:12 +0300

    init: start implementing doubly linked list

---
+++ doublyLinkedList.js
@@ -0,0 +1,33 @@
+class DoublyLinkedList {
+  constructor(...values) {
+
+  }
+
+  add(value) {
+
+  }
+
+  addByIndex(value, index) {
+
+  }
+
+  remove() {
+
+  }
+
+  removeByIndex(index) {
+
+  }
+
+  getNode(index) {
+
+  }
+}
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
